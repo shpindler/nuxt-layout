f162efdc33d9dc091cf41976c09e017dd600c3ee
Александр Шпиндлер <a.shpindler@bars.group>
2020-01-26 15:21:55 +0700

    Breakpoints injection added, mixins for devices added.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import path from 'path'
 
-export default function () {
+export default function (moduleOptions) {
+  const options = { ...moduleOptions, ...this.options.breakpoints }
   this.options.css.push(path.resolve(__dirname, 'index.scss'))
+  this.addPlugin({
+    src: path.resolve(__dirname, 'plugin.js'),
+    options: {
+      xs: options.xs || 576,
+      sm: options.sm || 768,
+      md: options.md || 992,
+      lg: options.lg || 1200
+    }
+  })
 }
---
+++ plugin.js
@@ -0,0 +1,98 @@
+/** Inject $bp attribute to Vue instance which consists breakpoints. */
+
+import Vue from 'vue'
+
+const xs = parseInt('<%= options.xs %>')
+const sm = parseInt('<%= options.sm %>')
+const md = parseInt('<%= options.md %>')
+const lg = parseInt('<%= options.lg %>')
+
+class BreakpointStore {
+  constructor() {
+    this.VM = new Vue({
+      data() {
+        return {
+          breakpoints: {
+            xs: [0, xs - 1],
+            sm: [xs, sm - 1],
+            md: [sm, md - 1],
+            lg: [md, lg - 1],
+            xl: [lg, Infinity]
+          },
+          screenWidth: document.documentElement.clientWidth
+        }
+      },
+      computed: {
+        bp() {
+          let result = null
+
+          forOwn(this.breakpoints, (v, k) => {
+            if (v[0] <= this.screenWidth && v[1] >= this.screenWidth) {
+              result = k
+            }
+          })
+
+          return result
+        },
+        xs() {
+          return this.bp === 'xs'
+        },
+        sm() {
+          return this.bp === 'sm'
+        },
+        md() {
+          return this.bp === 'md'
+        },
+        lg() {
+          return this.bp === 'lg'
+        },
+        xl() {
+          return this.bp === 'xl'
+        },
+        isMobile() {
+          return this.xs || this.sm
+        },
+        isTablet() {
+          return this.md
+        },
+        isDesktop() {
+          return this.lg || this.xl
+        },
+        isMobileOrTablet() {
+          return this.isMobile || this.isTablet
+        },
+        isDesktopOrTablet() {
+          return this.isDesktop || this.isTablet
+        }
+      },
+      created() {
+        addEventListener('resize', this.updateScreenWidth)
+        addEventListener('orientationchange', this.updateScreenWidth)
+      },
+      methods: {
+        updateScreenWidth() {
+          this.screenWidth = document.documentElement.clientWidth
+        }
+      }
+    })
+  }
+
+  get value() {
+    return this.VM.bp
+  }
+}
+
+const DevicePlugin = {
+  Store: BreakpointStore,
+  install(Vue, options) {
+    Vue.mixin({
+      beforeCreate() {
+        this.$bp = options.store.VM
+      }
+    })
+  }
+}
+
+Vue.use(DevicePlugin, {
+  store: new DevicePlugin.Store()
+})
